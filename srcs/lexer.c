3bdd05b6a17236acc0d75141b836707a1b15f5b1
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-01-06 22:39:23 +0900

    update

--- srcs/lexer.c
+++ srcs/lexer.c
@@ -6,42 +6,11 @@
 /*   By: tsekiguc <tsekiguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 20:51:38 by tsekiguc          #+#    #+#             */
-/*   Updated: 2022/01/06 22:13:04 by tsekiguc         ###   ########.fr       */
+/*   Updated: 2022/01/06 22:37:32 by tsekiguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
-
-char	*ft_strndup(char *str, size_t n)
-{
-	char	*ret;
-	size_t	len;
-	size_t	i;
-
-	if (str == NULL || n == 0)
-		return (NULL);
-	len = ft_strlen(str);
-	if (len > n)
-		len = n;
-	ret = (char *)malloc(sizeof(char) * (len + 1));
-	if (ret == NULL)
-		return (NULL);
-	i = 0;
-	while (str[i] != '\0' && i < n)
-	{
-		ret[i] = str[i];
-		i++;
-	}
-	ret[i] = '\0';
-	return (ret);
-}
-
-int	is_space(char c)
-{
-	if (c == ' ')
-		return (TRUE);
-	return (FALSE);
-}
 
 int	is_delimiter(char c)
 {
@@ -87,29 +56,25 @@ void	*lexer(t_list **list, char *cmd)
 	flag = 0;
 	while (cmd[i] != '\0' )
 	{
-		if (!is_space(cmd[i]) && !is_delimiter(cmd[i]))
+		if (!ft_isspace(cmd[i]) && !is_delimiter(cmd[i]))
 		{
 			flag = 1;
 			i++;
 		}
-		else if (is_space(cmd[i]) && flag == 1)
+		else if (ft_isspace(cmd[i]))
 		{
-			tmp = ft_substr(cmd, start, i - start);
-			//printf("tmp is %s\n", tmp);
-			ft_lstadd_back(list, ft_lstnew(tmp));
-			i++;
-			start = i;
-			flag = 0;
-		}
-		else if (is_space(cmd[i]) && flag == 0)
-		{
+			if (flag == 1)
+			{
+				tmp = ft_substr(cmd, start, i - start);
+				ft_lstadd_back(list, ft_lstnew(tmp));
+				flag = 0;
+			}
 			i++;
 			start = i;
 		}
 		else if (is_delimiter(cmd[i]) && flag == 1)
 		{
 			tmp = ft_substr(cmd, start, i - start);
-			//printf("tmp is %s\n", tmp);
 			ft_lstadd_back(list, ft_lstnew(tmp));
 			if ((cmd[i] == '<' && cmd[i + 1] == '<')
 				|| (cmd[i] == '>' && cmd[i + 1] == '>'))
@@ -119,9 +84,9 @@ void	*lexer(t_list **list, char *cmd)
 			}
 			else
 				ft_lstadd_back(list, ft_lstnew(ft_strndup(&cmd[i], 1)));
+			flag = 0;
 			i++;
 			start = i;
-			flag = 0;
 		}
 		else if (is_delimiter(cmd[i]) && flag == 0)
 		{
@@ -140,7 +105,6 @@ void	*lexer(t_list **list, char *cmd)
 	if (start < i)
 	{
 		tmp = ft_substr(cmd, start, i - start);
-		//printf("tmp is %s\n", tmp);
 		ft_lstadd_back(list, ft_lstnew(tmp));
 	}
 	return (list);
@@ -181,13 +145,6 @@ int main(void)
 	test("");
 	test("");
 	test("");
-
-
-
-
-
-
-
 	system("leaks minishell");
 	return (0);
 }
