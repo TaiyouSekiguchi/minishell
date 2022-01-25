c25dcf936f6e59899c3e645dcba197c53b3ed37f
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-01-25 15:16:57 +0900

    add quote_remove

--- srcs/expander.c
+++ srcs/expander.c
@@ -6,11 +6,74 @@
 /*   By: tsekiguc <tsekiguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 16:54:58 by tsekiguc          #+#    #+#             */
-/*   Updated: 2022/01/25 14:19:20 by tsekiguc         ###   ########.fr       */
+/*   Updated: 2022/01/25 15:15:59 by tsekiguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+char	*remove_quotation(char *token)
+{
+	char	*ret;
+	size_t	len;
+	size_t	i;
+	size_t	j;
+	t_quote	quote;
+
+	len = ms_strlen(token);
+	ret = (char *)ms_xmalloc(sizeof(char) * (len + 1));
+	quote = NONE;
+	i = 0;
+	j = 0;
+	while (token[i] != '\0')
+	{
+		if (quote == NONE)
+		{
+			if (is_quote(token[i]))
+			{
+				quote_set(token[i], &quote);
+				i++;
+			}
+			else
+			{
+				ret[j] = token[i];
+				i++;
+				j++;
+			}
+		}
+		else if (quote == SINGLE)
+		{
+			if (token[i] == '\'')
+			{
+				quote_set(token[i], &quote);
+				i++;
+			}
+			else
+			{
+				ret[j] = token[i];
+				i++;
+				j++;
+			}
+		}
+		else if (quote == DOUBLE)
+		{
+			if (token[i] == '\"')
+			{
+				quote_set(token[i], &quote);
+				i++;
+			}
+			else
+			{
+				ret[j] = token[i];
+				i++;
+				j++;
+			}
+		}
+	}
+	ret[j] = '\0';
+	free(token);
+	return (ret);
+}
 
 static void	expand_cmd_member(t_list **list)
 {
@@ -22,6 +85,7 @@ static void	expand_cmd_member(t_list **list)
 	{
 		token = current->content;
 		expand(&token);
+		token = remove_quotation(token);
 		current->content = token;
 		current = current->next;
 	}
