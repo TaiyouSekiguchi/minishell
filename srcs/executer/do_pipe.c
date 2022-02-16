074252d7e383b8265b018bb522be94095ae0b8c0
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-02-16 15:01:02 +0900

    リダイレクトのためのファイルオープン場所を修正

--- srcs/executer/do_pipe.c
+++ srcs/executer/do_pipe.c
@@ -6,7 +6,7 @@
 /*   By: tsekiguc <tsekiguc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 15:48:22 by tsekiguc          #+#    #+#             */
-/*   Updated: 2022/02/16 13:23:50 by tsekiguc         ###   ########.fr       */
+/*   Updated: 2022/02/16 15:00:40 by tsekiguc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,15 @@ pipe_handler(int fd_1, int fd_2, int io)
 	close(fd_1);
 	dup2(fd_2, io);
 	close(fd_2);
+}
+
+static t_kind
+distinguish_redirect_kind(char	*str)
+{
+	if (str[0] == '<' && str[1] == '<')
+		return (HEREDOC);
+	else
+		return (INFILE);
 }
 
 char
@@ -33,45 +42,19 @@ void
 heredoc_loop(int fd, char *token)
 {
 	char	*line;
-	//int		result;
 
 	while(1)
 	{
-		//write(STDOUT, "heredoc > ", 10);
-		//result = get_next_line(STDIN, &line);
 		line = rl_gets_heredoc();
 		if (ms_strcmp(line, &token[3]) == 0)
 			break ;
-		write(fd, line, ms_strlen(line));
-		write(fd, "\n", 1);
+		ms_putendl_fd(line, fd);
 		free(line);
-		/*if (result == ERROR)
-		{
-			close(fd);
-			ms_error("gnl failed");
-		}
-		else if (result == END)
-			break ;
-		else
-		{
-			if (ms_strcmp(line, &token[3]) == 0)
-				break ;
-			write(fd, line, ms_strlen(line));
-			write(fd, "\n", 1);
-		}*/
 	}
 }
 
-static t_kind
-distinguish_redirect_kind(char	*str)
-{
-	if (str[0] == '<' && str[1] == '<')
-		return (HEREDOC);
-	else
-		return (INFILE);
-}
-
-int	file_open(char *token)
+int
+file_open(char *token)
 {
 	t_kind	kind;
 	int		fd;
@@ -79,19 +62,18 @@ int	file_open(char *token)
 	kind = distinguish_redirect_kind(token);
 	if (kind == INFILE)
 	{
-		printf("this is infile zone\n");
+		//printf("File open kind : INFILE\n");
 		fd = open(&token[2], O_RDONLY);
 		if (fd < 0)
 			ms_error("open");
 	}
 	else
 	{
-		printf("this is heredoc zone\n");
+		//printf("File open kind : HEREDOC\n");
 		fd = open("./tmp", O_WRONLY | O_CREAT | O_EXCL | O_TRUNC, 0600);
 		if (fd < 0)
 			ms_error("open");
 
-		//exec heredoc
 		heredoc_loop(fd, token);
 		close(fd);
 
@@ -105,6 +87,26 @@ int	file_open(char *token)
 	return (fd);
 }
 
+int		get_fd(t_cmd *cmd_group)
+{
+	int		fd;
+	t_list	*infile;
+	char	*token;
+
+	infile = cmd_group->infile;
+	token = NULL;
+
+	if (infile != NULL)
+		token = infile->content;
+	printf("token : %s\n", token);
+	if (token != NULL)
+		fd = file_open(token);
+	else
+		fd = -1;
+
+	return (fd);
+}
+
 
 void
 do_pipe(t_list *cmds, int fd)
@@ -112,20 +114,18 @@ do_pipe(t_list *cmds, int fd)
 	int		pp[2];
 	pid_t	ret;
 	t_list	*prev_cmds;
-	t_cmd	*prev_cmd;
-	t_list	*infile;
+	t_cmd	*prev_cmd_group;
 	char	*token;
-	t_cmd	*cmd;
+	t_cmd	*cmd_group;
 
+	token = NULL;
 	if (cmds->prev == NULL)
 	{
-		printf("here1\n");
-		if (fd != -1)
+
+		if (fd < 0)
 		{
-			cmd = cmds->content;
-			infile = cmd->infile;
-			token = infile->content;
-			fd = file_open(token);
+			cmd_group = cmds->content;
+			fd = get_fd(cmd_group);
 		}
 
 		do_exec(cmds->content, fd);
@@ -136,41 +136,21 @@ do_pipe(t_list *cmds, int fd)
 		ret = fork();
 		if (ret == 0)
 		{
-			printf("here2\n");
-			//cat << end
-			//file open  heredocument
 			prev_cmds = cmds->prev;
-			prev_cmd = prev_cmds->content;
-			infile = prev_cmd->infile;
-			token = infile->content;
-			printf("token : %s\n", token);
-
-			fd = file_open(token);
-			printf("fd : %d\n", fd);
+			prev_cmd_group = prev_cmds->content;
+			fd = get_fd(prev_cmd_group);
 
 			pipe_handler(pp[0], pp[1], 1);
-			/*close(pp[0]);
-			dup2(pp[1], 1);
-			close(pp[1]);*/
 			do_pipe(cmds->prev, fd);
 		}
 		else
 		{
-			printf("here3\n");
-			//grep << END
 			wait(NULL);
-			cmd = cmds->content;
-			infile = cmd->infile;
-			token = infile->content;
-			printf("token : %s\n", token);
-			
-			fd = file_open(token);
-			printf("fd : %d\n", fd);
+
+			cmd_group = cmds->content;
+			fd = get_fd(cmd_group);
 
 			pipe_handler(pp[1], pp[0], 0);
-			/*close(pp[1]);
-			dup2(pp[0], 0);
-			close(pp[0]);*/
 			do_exec(cmds->content, fd);
 		}
 	}
