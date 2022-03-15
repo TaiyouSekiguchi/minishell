19c0aab67e256b216eaaae92f47d422df20fd02b
tsekiguc <tsekiguc@student.42tokyo.jp>
2022-03-15 17:00:28 +0900

    expand_cmd_info_element関数内のフリーを修正しました

--- srcs/expander/expander.c
+++ srcs/expander/expander.c
@@ -1,11 +1,13 @@
 #include "minishell.h"
 
-static t_list	*expand_cmd_info_element(t_list *current, char **my_env)
+static t_list	*expand_cmd_info_element(t_list *list, char **my_env)
 {
 	t_list	*new_lst;
+	t_list	*current;
 	char	*token;
 
 	new_lst = NULL;
+	current = list;
 	while (current != NULL)
 	{
 		token = current->content;
@@ -32,15 +34,15 @@ static t_list	*expand_cmd_info_element(t_list *current, char **my_env)
 		}
 		current = current->next;
 	}
-	ms_lstclear(&current, free);
+	ms_lstclear(&list, free);
 	return (new_lst);
 }
 
-static void	expand_cmd_info(t_cmd_info **cmd_info, char **my_env)
+/*static void	expand_cmd_info(t_cmd_info **cmd_info, char **my_env)
 {
 	(*cmd_info)->cmd = expand_cmd_info_element((*cmd_info)->cmd, my_env);
 	(*cmd_info)->redirect = expand_cmd_info_element((*cmd_info)->redirect, my_env);
-}
+}*/
 
 void	expander(t_list *cmd_info_list, char **my_env)
 {
@@ -51,7 +53,9 @@ void	expander(t_list *cmd_info_list, char **my_env)
 	while (current != NULL)
 	{
 		cmd_info = current->content;
-		expand_cmd_info(&cmd_info, my_env);
+		cmd_info->cmd = expand_cmd_info_element(cmd_info->cmd, my_env);
+		cmd_info->redirect = expand_cmd_info_element(cmd_info->redirect, my_env);
+		//expand_cmd_info(&cmd_info, my_env);
 		current = current->next;
 	}
 }
